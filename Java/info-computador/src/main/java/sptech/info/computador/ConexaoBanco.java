58d0206cf707633f10e464cd70010c1e8d251c6b
NathanD4vid <nathan.silva@sptech.school>
2023-04-05 11:07:56 -0300

    JAR inicial inserindo no banco

--- Java/info-computador/src/main/java/sptech/info/computador/ConexaoBanco.java
+++ Java/info-computador/src/main/java/sptech/info/computador/ConexaoBanco.java
@@ -9,7 +9,6 @@ import com.github.britooo.looca.api.group.discos.Volume;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
