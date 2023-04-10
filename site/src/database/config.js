6eb270b389181b4d4a1fc8ee9c0e5156385c4641
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2023-04-09 21:09:47 -0300

    Arrumando pastas para o deploy

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "servidor-monitor-mind.database.windows.net",
+    database: "bd-monitor-mind",
+    user: "servidor-monitor-mind.database.windows.net",
+    password: "#Gfgrupo4",
     pool: {
         max: 10,
         min: 0,
