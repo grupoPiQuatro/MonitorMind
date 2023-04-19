12c693cbf6478ed2098a98c149b5eb6156b1ab23
NathanD4vid <nathan.silva@sptech.school>
2023-04-19 12:39:24 -0300

    tela computador 70%

--- site/src/database/script_Monitor-Mind.sql
+++ site/src/database/script_Monitor-Mind.sql
@@ -14,7 +14,7 @@ nomeUsuario varchar (45),
 email varchar (45),
 senha varchar (45),
 tipo varchar(6),
-constraint ctTipo check (tipo in ('Owner', 'Admin', 'Normal')),
+constraint ctTipo check (tipo in ('Admin', 'Normal')),
 fkEmpresa int, 
 constraint ctFkEmpresaUser foreign key (fkEmpresa) references Empresa (idEmpresa),
 primary key (idUsuario, fkEmpresa)
@@ -29,33 +29,48 @@ constraint ctFkEmpresaEnd foreign key (fkEmpresa) references Empresa (idEmpresa)
 primary key (idEndereco, fkEmpresa)
 ); 
 
+create table Localizacao (
+idLocalizacao int primary key auto_increment,
+setor varchar(45),
+andar varchar(45)
+);
+
+create table Componente (
+idComponente int primary key auto_increment,
+tipo varchar(45),
+numeroChave double,
+unidadeMedida varchar(45)
+);
+
 create table Computador (
 serialComputador varchar(15) primary key,
 sistemaOperacional varchar(45),
-nomeCpu varchar(45),
-nucleoFisico int,
-nucleoLogico int,
-qtdRam long,
-qtdArmazenamento long,
-tipoDisco varchar(3),
 status varchar(15),
 constraint ctStatus check (status in ('Operando', 'Manutenção', 'Interrompido')),
+fkLocalizacao int,
+constraint ctFkLocalizacao foreign key (fkLocalizacao) references Localizacao (idLocalizacao),
 fkEmpresa int,
 constraint ctFkEmpresa foreign key (fkEmpresa) references Empresa (idEmpresa)
 );
-
-drop database monitormind;
 
 select * from Empresa;
 select * from Endereco;
 select * from Usuario where email = '123' and senha = '123';
 select * from Computador;
+select * from Componente;
+select * from Localizacao;
 
 truncate empresa;
 truncate endereco;
 truncate usuario;
 truncate computador;
+truncate componente;
+truncate Localizacao;
+
+drop table computador;
+drop table localizacao;
 
 INSERT INTO Usuario (nomeUsuario, email, senha, tipo, fkEmpresa) VALUES ('Nathan David','123', '123', 'Owner', 1);
 INSERT INTO Empresa (nomeEmpresa, cnpj, telefone) values ('Contax', '12345678901234', '1187878787');
 INSERT INTO Endereco (cep, numero, fkEmpresa) VALUES ('05163020', '8', 1);
+INSERT INTO Componente (tipo, numeroChave, unidadeMedida) values ('rede', null, 'ms');
